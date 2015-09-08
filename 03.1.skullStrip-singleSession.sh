12c64cd02d84340c4173e85b2e7388174fa12a0e
Stephen Towler <stowler@gmail.com>
2015-09-08 19:10:03 -0400

    migrated skull strip scripts from off-line

---
+++ 03.1.skullStrip-singleSession.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# skull-strip a single session
+
+# This file is just here as a placeholder to point to the script to use for actual brain extraction:
+
+# $bwDir/bwSkullStrip-singleSession.sh
+
+# ...which gets called with a Hackney sessionDir as its single argument, e.g.,
+# $bwDir/bwSkullStrip-singleSession.sh \
+# /data/panolocal/processedOnPano-hackney/derivedData/omt001s01
---
+++ 03.2.skullStrip-parallelSessions.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# skull-strip multiple sessions in parallel
+
+# This file is just here as a place-holder to point to the script to use for actual brain extractions:
+# $bwDir/bwSkullStrip-parallelSessions.sh
+
+# ...which gets called with hackney niftiDirProject as its first argument, and
+# number of parallel operations as its second, e.g.,
+#
+# $bwDir/bwSkullStrip-parallelSessions.sh \
+# /data/panolocal/processedOnPano-hackney/derivedData \
+# 20
