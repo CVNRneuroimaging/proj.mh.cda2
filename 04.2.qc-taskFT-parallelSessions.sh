ffa7ce269b04e1e2971893f810f36dd2a8ac38cf
Stephen Towler <stowler@gmail.com>
2015-09-09 12:02:17 -0400

    moved parallelQcRuns to command-line argument

--- 04.2.qc-taskFT-parallelSessions.sh
+++ 04.2.qc-taskFT-parallelSessions.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-parallelQcRuns=20
+parallelQcRuns=$1
 niftiDirProject=/data/panolocal/processedOnPano-hackney/derivedData
 
 ls -d ${niftiDirProject}/* | parallel --jobs ${parallelQcRuns} --tag --line-buffer ~stowler-local/src.mywork.gitRepos/proj.mh.cda2/04.1.qc-taskFT-singleSession.sh {}
