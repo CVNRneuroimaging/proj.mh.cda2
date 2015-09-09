8b401348df01c21dd1e88c8de74f0a1895d97160
Stephen Towler <stowler@gmail.com>
2015-09-09 07:38:05 -0400

    migrated from off-line: scripts for serial and parallel FIX

---
+++ 08.1.fix-singleRun.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# 08.1.fix-singleRun.sh
+#
+# Applying FIX to a single melodic output directory should take fewer than 10 minutes on most modern hardware:
+
+############# arguments: ############
+#...structured to allow this script to be easly called by external parllel
+# processor such as GNU parallel or ppss
+#
+# TBD: test passed values
+
+# the melodic output directory that we'll copy for fix i/o
+# example: /data/panolocal/processedOnPano-hackney/derivedData/omt001s01/omt001s01.fmri.taskFT.run1.melodicFixNone.ica
+melodicOut=$1
+
+# the existing trained-weights file that will be used to classify components
+# example: /opt/fix/training_files/Standard.RData
+fixWeightsFile=$2
+
+# the fix threshold
+# example: 20
+fixThresh=$3
+
+# how this script is being executed. just affects output naming.
+# example: serial, parallel.gnu, parallel.ppss, parallel.tmux
+execution=$4
+
+# get basename so it can be used in stdout/logs:
+fixInName="`basename ${melodicOut}`"
+
+# construct name of fix input/output dir
+fixWeightsName=`basename ${fixWeightsFile} | sed 's/\.RData//'`
+melodicBasename="`basename ${melodicOut} | sed 's/\.melodicFixNone\.ica//'`"              # ...e.g., "omt001s01.fmri.taskFT.run1"
+fixDirName=${melodicBasename}.melodicFix${fixWeightsName}${fixThresh}-${execution}.ica    # ...e.g., "omt001s01.fmri.taskFT.run1.melodicFixStandard20-gnuParallel18.ica"
+
+# the fix input/output directory will be sibling to the original melodic directory:
+fixDirParent="`dirname ${melodicOut}`"
+
+# Create a copy of the original non-fix melodic .ica output. Will use the copy for fix input/output:
+cp -r ${melodicOut} ${fixDirParent}/${fixDirName}
+
+# run fix:
+echo ""
+echo "######################################"
+date
+echo "Launching FIX:"
+echo "MELODIC dir:      ${fixInName}"
+echo "trained-weights:  ${fixWeightsName}"
+echo "FIX threshold:    ${fixThresh}"
+echo -n "FIX version:      "
+which fix | xargs dirname | xargs ls -l
+echo "######################################"
+echo ""
+/usr/bin/time fix ${fixDirParent}/${fixDirName} ${fixWeightsFile} ${fixThresh} -m
+echo ""
+date
+echo "FIX complete for thresh ${fixThresh} of ${fixInName}."
+du -sh ${fixDirParent}/${fixDirName}
+echo ""
---
+++ 08.2.fix-taskFT-parallelRuns.sh
@@ -0,0 +1,81 @@
+#!/bin/sh
+
+# 08.2.fix-taskFT-parallelRuns.sh
+#
+# Applies parallel application of FIX to MELODIC .ica directories via gnu parallel calls to 08.1.fix-singleRun.sh
+# Can be used for fmri rest or fmri taskFT (see below).
+
+##############
+# INPUTS
+#############
+#
+# Arguments that can be manipulated between executions:
+#
+#     The argument after "ls -d" controls what melodic directories will serve as inputs to FIX:
+#        ls -d ${niftiDirProject}/omt*/*taskFT*melodicFixNone.ica     #...for fmri task runs
+#        ls -d ${niftiDirProject}/omt*/*rest*melodicFixNone.ica       #...for fmri resting state runs
+#
+#
+#     The penultimate argument controls FIX threshold:
+#        20
+#        18
+#        15
+#        10
+#
+#
+#     The third-from-last argument specifies the FIX trained-weights dataset that should be used:
+#        /opt/fix/training_files/HCP_hp2000.RData
+#        /opt/fix/training_files/Standard.RData
+#        /opt/fix/training_files/UKBiobank.RData
+#        /opt/fix/training_files/WhII_MB6.RData
+#        /opt/fix/training_files/WhII_Standard.RData
+#
+##############
+# OUTPUTS 
+#############
+#
+# New FIX-corrected melodic .ica directory for each input run.  See
+# 08.1.fix-singleRun.sh , which parses FIX parameters to name its new
+# FIX-corrected .ica directories.
+#
+
+
+# Get the number of parallel jobs from the command line, or define internally:
+#parallelFixRuns=$1
+parallelFixRuns=10
+
+
+# The project root containing melodic .ica directories:
+niftiDirProject=/data/panolocal/processedOnPano-hackney/derivedData
+
+
+# Specify FIX parameters. 
+# NB: not receiving $fixThresh from command line argument because of potential
+# confusion with commandline argument for $parallelFixRuns:
+fixWeightsFile=/opt/fix/training_files/Standard.RData
+fixThresh=20
+
+
+# Suffix for output .ica directory indicating how processing was performed
+# e.g., "gnuParallel10rama" for 10 simultaneous gnu parallel jobs executed on rama
+# (arbitrary string...just a way to help troubleshoot output when parallel processing goes awry)
+executionSuffix=gnuParallel10
+
+echo ""
+echo "###################################################################"
+echo "Launching parallel executions of 08.1.fix-singleRun.sh "
+echo ""
+echo "parallelFixRuns    : $parallelFixRuns"
+echo "fixWeightsFile     : $fixWeightsFile"
+echo "fixThresh          : $fixThresh"
+echo "executionSuffix    : $executionSuffix"
+echo "###################################################################"
+echo ""
+
+# Use gnu parallel to execute for either (but not both simultaneously):
+#
+# ...fmri taskFT runs:
+ls -d ${niftiDirProject}/omt*/*taskFT*melodicFixNone.ica | parallel --jobs ${parallelFixRuns} --tag --line-buffer ~stowler-local/src.mywork.gitRepos/proj.mh.cda2/08.1.fix-singleRun.sh {} ${fixWeightsFile} ${fixThresh} ${executionSuffix}
+#
+# ...or fmri rest runs:
+#ls -d ${niftiDirProject}/omt*/*rest*melodicFixNone.ica   | parallel --jobs ${parallelFixRuns} --tag --line-buffer ~stowler-local/src.mywork.gitRepos/proj.mh.cda2/08.1.noceraFix-singleRun.sh {} ${fixWeightsFile} ${fixThresh} ${executionSuffix}
