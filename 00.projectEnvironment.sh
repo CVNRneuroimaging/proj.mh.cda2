3dd82aed59bee15859b6325fd45f8381c5689b54
Stephen Towler <stowler@gmail.com>
2015-09-24 22:06:34 -0400

    session list for target participants right leg pre sessions

--- 00.projectEnvironment.sh
+++ 00.projectEnvironment.sh
@@ -12,8 +12,9 @@ export scriptDir="~stowler-local/src.mywork.gitRepos/proj.mh.cda2"
 # "atypical" sessions 
 # (per the definition of typical, further down)
 #
-# omt022s01 - missing fmri.taskFT.run1 (TBD: recreate...had good run1 plus short-TR false start)
 # omt016s01 - eprime run3 died per MH
+# omt022s01 - missing fmri.taskFT.run1 (TBD: recreate...had good run1 plus short-TR false start, move to typical lists)
+# omt221s01 - missing anat because dicom store has two copies of anat (TBD: recreate from single, move to typical lists)
 # 
 
 ###########################################################
@@ -30,9 +31,10 @@ export scriptDir="~stowler-local/src.mywork.gitRepos/proj.mh.cda2"
 # - omt2XXsXX - session IDs for target particpants acting as treatment "followers"
 # - omt3XXsXX - session IDs for target particpants acting as treatment "educational controls"
 
-sessionsTypicalHOAllegPre="omt027"
+sessionsTypicalHOAllegPre="omt027s01"
 sessionsTypicalHOArlegPre="omt001s01 omt003s01 omt004s01 omt006s01 omt007s01 omt008s01 omt009s01 omt010s01 omt011s01 omt012s01 omt013s01 omt015s01 omt021s01 omt023s01 omt024s01 omt025s01 omt028s01 omt029s01 omt030s01 omt031s01"
-sessionsTypicalTGTrlegPre=
+sessionsTypicalTGTrlegPre="omt102s01 omt103s01 omt105s01 omt108s01 omt110s01 omt111s01 omt115s01 omt120s01 omt121s01 omt122s01 omt202s01 omt203s01 omt206s01 omt207s01 omt220s01 omt221s01 omt226s01 omt312s01 omt314s01 omt317s01 omt318s01 omt320s01"
+
 #sessionsTypicalTGTlPre=
 
 #######################################################################
@@ -50,3 +52,5 @@ sessionsTypicalTGTrlegPre=
 # convert lists from rows to columns for other use:
 export sessionsTypicalHOArlegPre_column="`echo ${sessionsTypicalHOArlegPre} | sed 's/ /\n/g'`"
 #echo "$sessionsTypicalHOArlegPre_column"
+export sessionsTypicalTGTrlegPre_column="`echo ${sessionsTypicalTGTrlegPre} | sed 's/ /\n/g'`"
+echo "$sessionsTypicalTGTrlegPre_column"
