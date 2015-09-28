84efd1e3519606b21c6de802aaea1cd4776d27d8
Stephen Towler <stowler@gmail.com>
2015-09-28 10:47:07 -0400

    reduced fixThresh from 18 to 15

--- 08.3.fix-rest-parallelRuns.sh
+++ 08.3.fix-rest-parallelRuns.sh
@@ -54,7 +54,7 @@ niftiDirProject=/data/panolocal/processedOnPano-hackney/derivedData
 # NB: not receiving $fixThresh from command line argument because of potential
 # confusion with commandline argument for $parallelFixRuns:
 fixWeightsFile=/opt/fix/training_files/WhII_Standard.RData
-fixThresh=18
+fixThresh=15
 
 
 # Suffix for output .ica directory indicating how processing was performed
